e1437cd936707d1038bcd47104a90b42ab51767b
NobChanvisal <chanvisal.nob09@gmail.com>
2024-03-30 01:44:20 +0700

    update code

--- GUISwing/GUI/src/mar/mar_28_24/buttons.java
+++ GUISwing/GUI/src/mar/mar_28_24/buttons.java
@@ -1,6 +1,7 @@
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.border.Border;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -8,6 +9,7 @@ import java.awt.event.ActionListener;
 
 public class buttons extends JFrame {
     JButton button = new JButton();
+    JLabel label = new JLabel();
     Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
 
     buttons() {
@@ -17,7 +19,14 @@ public class buttons extends JFrame {
         setVisible(true);
         setLayout(null);
         add(button);
-
+        add(label);
+        
+        // Create label
+        label.setBounds(200, 200, 200, 35); // Adjust position and size of the label
+        label.setText("buttons clicked!");
+        label.setVisible(false);
+        
+        // Create button
         button.setBounds(200, 100, 150, 35);
         button.setText("Click Me");
         button.setFocusable(false);
@@ -26,7 +35,9 @@ public class buttons extends JFrame {
         // Add ActionListener to the button
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Button clicked!");
+                // Set the label text when the button is clicked
+                label.setVisible(true);
+                System.out.println("buttons clicked!");
             }
         });
     }
---
+++ GUISwing/GUI/src/mar/mar_29_24/BorderLayouts.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class BorderLayouts extends JFrame {
+    JPanel redPanel = new JPanel();
+    JPanel bluePanel = new JPanel();
+    JPanel yellowPanel = new JPanel();
+    JPanel greenPanel = new JPanel();
+    JPanel pinkPanel = new JPanel();
+    
+    BorderLayouts() {
+        super("Border Layouts");
+        setSize(500, 500);
+        setLayout(new BorderLayout(10,10));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        redPanel.setBackground(Color.red);
+        bluePanel.setBackground(Color.blue);
+        yellowPanel.setBackground(Color.yellow);
+        greenPanel.setBackground(Color.green);
+        pinkPanel.setBackground(Color.pink);
+
+        redPanel.setPreferredSize(new Dimension(100, 50));
+        bluePanel.setPreferredSize(new Dimension(100, 50));
+        yellowPanel.setPreferredSize(new Dimension(100, 50));
+        greenPanel.setPreferredSize(new Dimension(100, 50));
+        pinkPanel.setPreferredSize(new Dimension(100, 50));
+
+        // Adding panels to different regions of the BorderLayout
+        add(redPanel, BorderLayout.NORTH);
+        add(bluePanel, BorderLayout.SOUTH);
+        add(yellowPanel, BorderLayout.WEST);
+        add(greenPanel, BorderLayout.EAST);
+        add(pinkPanel, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+    
+            new BorderLayouts();  
+
+    }
+}
+
